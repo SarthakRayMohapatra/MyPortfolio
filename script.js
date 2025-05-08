4703978c318dfd00bd5bf976dbcf68563d3ec152
SarthakRayMohapatra <sarthakraymohapatra225@gmail.com>
2025-05-09 00:56:16 +0530

    Finalize Contact section, integrate Style Switcher, & enhance site with JS

---
+++ script.js
@@ -0,0 +1,8 @@
+// Typing Animation
+
+var typed = new Typed(".typing",{
+    strings:["","Web Designer","Web Developer","Graphic Designer","Content Creator","Video Editor","YouTuber"],
+    typespeed:100,
+    BackSpeed:60,
+    loop:true
+})
